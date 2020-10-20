04e28b56e3689a17a7112162c920747ce7adbe8f
Amit Gupta <amit@gprof.com>
2020-10-19 18:24:29 -0700

    Initial Commit

---
+++ COVID_SwiftManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID_SwiftManager
+//
+//  Created by Amit Gupta on 10/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
